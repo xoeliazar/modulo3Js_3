ef0029edab6e239fd9151c0c04abe0afc0ddb821
Sergio Felipe Eliazar Salcedo López <136506920+xoeliazar@users.noreply.github.com>
2024-04-13 23:41:33 -0400

    Ahora si se guardan comentarios

--- requerimiento5/assets/js/script.js
+++ requerimiento5/assets/js/script.js
@@ -1,3 +1,17 @@
+// ● Crea una página junto a un script que guarde dentro de una variable global
+// un color dependiendo de la letra del teclado presionada. (2 Puntos).
+// ○ La letra a guardará el color rosado.
+// ○ La letra s guardará el color naranjo.
+// ○ La letra d guardará el color celeste.
+// ○ Para guardar el color revisa el tip al final del enunciado.
+// ○ Crea un nuevo div con el id “key”, de 200 px de ancho y alto y de
+// color blanco y borde negro.
+// ○ Al presionar las teclas a, s o d, se deberá cambiar el color del div
+// “key” a rosado, naranjo o celeste respectivamente.
+// ● Siguiendo con la lógica del punto anterior, al presionar las teclas q, w o e se
+// deberá crear un div nuevo de las mismas dimensiones antes mencionadas
+// con los colores morado, gris y café respectivamente. (1 Punto)
+
 const color = "";
 
 const cambiarColor = (color) => {
